03ca3af21f97c26f899fb792be63a277e5cf4b29
Anthologia <pyoungjy@gmail.com>
2022-02-24 03:32:05 +0900

    스프링 핵심 원리 이해2 (객체 지향 원리 적용) - 관심사의 분리

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,11 @@ package hello.core.member;
 // 구현체가 하나인 경우에는 보통 끝에 Impl 을 붙인다
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
