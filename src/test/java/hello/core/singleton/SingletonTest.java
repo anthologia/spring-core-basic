4f089816db0a926d69db523b4fc9f9f2ada3e268
Anthologia <pyoungjy@gmail.com>
2022-02-26 23:19:08 +0900

    싱글톤 컨테이너 - 싱글톤 패턴

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -1,7 +1,9 @@
 package hello.core.singleton;
 
 import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
+import hello.core.member.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,5 +26,33 @@ public class SingletonTest {
 
         // memberService != memberService2
         Assertions.assertThat(memberService).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용하여 DIP를 만족하는 객체 사용")
+    void singletonDipMememoryMemberRepositoryTest() {
+        AppConfig appConfig = new AppConfig();
+
+        MemberRepository memberRepository1 = appConfig.MemberRepository();
+        MemberRepository memberRepository2 = appConfig.MemberRepository();
+
+        System.out.println("memberRepository1 = " + memberRepository1);
+        System.out.println("memberRepository2 = " + memberRepository2);
+
+        Assertions.assertThat(memberRepository1).isSameAs(memberRepository2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        // isSameAs() 메서드는 객체의 참조 주소값이 같은지 비교를 하고
+        // isEqualTo() 메서드는 객체가 가진 값이 같은지 비교를 한다
+        Assertions.assertThat(singletonService1).isSameAs(singletonService2);
     }
 }
