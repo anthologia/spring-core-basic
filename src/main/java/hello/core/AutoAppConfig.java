8b7c63a9bd970c27310f0b037647b11a7715fc16
Anthologia <pyoungjy@gmail.com>
2022-03-02 22:54:24 +0900

    컴포넌트 스캔 - 탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 // 이전에 만들었던 AppConfig를 포함하지 않기 위함
 @ComponentScan(
+        basePackages = "hello.core.member",
+        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
